2f51805dc2e8d61186ec254a1c686d8dd824d095
Muhammad Muntaza <muhammad@muntaza.id>
2020-01-19 11:45:47 +0800

    OK

--- saldo_awal_2019/rsud/sub_unit/ruang_icu/script_rubah.sh
+++ saldo_awal_2019/rsud/sub_unit/ruang_icu/script_rubah.sh
@@ -1,2 +1,2 @@
-cat depo_ok_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87657\,/g -e s/$/\)\;/g > depo_ok_masuk_insert.sql
-cat depo_ok_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87658\,/g -e s/$/\)\;/g > depo_ok_keluar_insert.sql
+cat ruang_icu_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(89249\,/g -e s/$/\)\;/g > ruang_icu_masuk_insert.sql
+cat ruang_icu_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(89250\,/g -e s/$/\)\;/g > ruang_icu_keluar_insert.sql
