8e93b8e6fc36da9086102e68c1fc082a9b8258ce
Muhammad Muntaza <muhammad@muntaza.id>
2020-01-19 09:31:28 +0800

    OK

--- distribusi_dinas_2019/rsud/distribusi_obat/script_rubah.sh
+++ distribusi_dinas_2019/rsud/distribusi_obat/script_rubah.sh
@@ -1,1 +1,1 @@
-cat masuk_distribusi_ke_apotik_rawat_jalan.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87046\,/g -e s/$/\)\;/g > masuk_distribusi_ke_apotik_rawat_jalan_insert.sql
+cat masuk_distribusi_ke_apotik_rawat_jalan.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(89243\,/g -e s/$/\)\;/g > masuk_distribusi_ke_apotik_rawat_jalan_insert.sql
