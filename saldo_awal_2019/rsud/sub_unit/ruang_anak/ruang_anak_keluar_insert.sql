aeb176f51455b0bf69b6132b0b8ef26340cdbc09
Muhammad Muntaza <muhammad@muntaza.id>
2020-01-21 17:43:03 +0800

    OK

---
+++ saldo_awal_2019/rsud/sub_unit/ruang_anak/ruang_anak_keluar_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89635,650,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89635,3976,1,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89635,2620,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89635,601,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89635,502,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89635,13290,3,0);
---
+++ saldo_awal_2019/rsud/sub_unit/ruang_anak/ruang_anak_masuk_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89633,650,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89633,3976,1,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89633,2620,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89633,601,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89633,502,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (89633,13290,3,0);
