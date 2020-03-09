ada00dde6841d07d21fd0319c2f9008fb46aec57
Muhammad Muntaza <muhammad@muntaza.id>
2020-03-09 22:31:53 +0800

    UPDATE, deploy Laporan 2020 di Production: Balangan

---
+++ script_sed_project_2020/edit_database.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# edit database pada file config.php
+# muhammad@muntaza.id
+
+sed -i 's/persediaan_2019/persediaan_2020/g' ./01_setwan/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./02_setda/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./03_dpupr/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./04_dishub/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./05_dinkes/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./06_blud_rsud/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./07_disdik/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./08_perpustakaan/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./09_sosial/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./10_dpmd/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./11_dpppa/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./12_dukcatpil/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./13_pertanian/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./15_dkp/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./16_dkukmp/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./18_dpmptsp/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./19_bkd/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./20_inspektorat/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./21_bappeda/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./22_dlh/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./23_dko/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./24_kesbangpol/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./25_satpolpp/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./26_bkppd/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./28_paringin/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./31_lampihong/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./32_batumandi/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./33_juai/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./34_awayan/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./35_halong/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./36_paringinselatan/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./38_tebingtinggi/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./39_bpbd/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./40_dpkp/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./41_disnakertrans/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./42_dppkb/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./43_kominfo/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./44_kearsipan/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./45_perikanan/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./46_pariwisata/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./47_perdagangan/config.php
+sed -i 's/persediaan_2019/persediaan_2020/g' ./48_bppd/config.php
---
+++ script_sed_project_2020/edit_laporan.sh
@@ -0,0 +1,250 @@
+#!/bin/bash
+
+# edit file laporan
+# muhammad@muntaza.id
+
+
+# Buku Penerimaan
+
+sed -i 's/2019/2020/g' ./01_setwan/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./02_setda/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./03_dpupr/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./04_dishub/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./05_dinkes/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./06_blud_rsud/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./07_disdik/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./08_perpustakaan/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./09_sosial/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./10_dpmd/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./11_dpppa/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./12_dukcatpil/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./13_pertanian/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./15_dkp/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./16_dkukmp/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./18_dpmptsp/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./19_bkd/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./20_inspektorat/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./21_bappeda/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./22_dlh/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./23_dko/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./24_kesbangpol/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./25_satpolpp/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./26_bkppd/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./28_paringin/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./31_lampihong/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./32_batumandi/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./33_juai/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./34_awayan/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./35_halong/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./36_paringinselatan/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./38_tebingtinggi/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./39_bpbd/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./40_dpkp/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./41_disnakertrans/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./42_dppkb/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./43_kominfo/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./44_kearsipan/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./45_perikanan/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./46_pariwisata/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./47_perdagangan/buku_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./48_bppd/buku_penerimaan_skpd.xml
+
+
+
+
+
+# Buku Pengeluaran
+
+
+sed -i 's/2019/2020/g' ./01_setwan/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./02_setda/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./03_dpupr/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./04_dishub/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./05_dinkes/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./06_blud_rsud/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./07_disdik/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./08_perpustakaan/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./09_sosial/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./10_dpmd/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./11_dpppa/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./12_dukcatpil/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./13_pertanian/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./15_dkp/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./16_dkukmp/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./18_dpmptsp/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./19_bkd/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./20_inspektorat/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./21_bappeda/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./22_dlh/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./23_dko/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./24_kesbangpol/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./25_satpolpp/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./26_bkppd/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./28_paringin/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./31_lampihong/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./32_batumandi/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./33_juai/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./34_awayan/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./35_halong/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./36_paringinselatan/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./38_tebingtinggi/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./39_bpbd/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./40_dpkp/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./41_disnakertrans/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./42_dppkb/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./43_kominfo/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./44_kearsipan/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./45_perikanan/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./46_pariwisata/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./47_perdagangan/buku_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./48_bppd/buku_pengeluaran_skpd.xml
+
+
+
+
+
+
+# Daftar Penerimaan
+
+sed -i 's/2019/2020/g' ./01_setwan/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./02_setda/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./03_dpupr/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./04_dishub/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./05_dinkes/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./06_blud_rsud/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./07_disdik/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./08_perpustakaan/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./09_sosial/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./10_dpmd/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./11_dpppa/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./12_dukcatpil/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./13_pertanian/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./15_dkp/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./16_dkukmp/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./18_dpmptsp/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./19_bkd/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./20_inspektorat/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./21_bappeda/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./22_dlh/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./23_dko/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./24_kesbangpol/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./25_satpolpp/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./26_bkppd/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./28_paringin/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./31_lampihong/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./32_batumandi/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./33_juai/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./34_awayan/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./35_halong/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./36_paringinselatan/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./38_tebingtinggi/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./39_bpbd/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./40_dpkp/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./41_disnakertrans/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./42_dppkb/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./43_kominfo/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./44_kearsipan/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./45_perikanan/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./46_pariwisata/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./47_perdagangan/daftar_penerimaan_skpd.xml
+sed -i 's/2019/2020/g' ./48_bppd/daftar_penerimaan_skpd.xml
+
+
+
+
+
+# Daftar Pengadaan
+
+
+sed -i 's/2019/2020/g' ./01_setwan/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./02_setda/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./03_dpupr/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./04_dishub/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./05_dinkes/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./06_blud_rsud/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./07_disdik/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./08_perpustakaan/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./09_sosial/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./10_dpmd/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./11_dpppa/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./12_dukcatpil/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./13_pertanian/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./15_dkp/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./16_dkukmp/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./18_dpmptsp/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./19_bkd/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./20_inspektorat/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./21_bappeda/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./22_dlh/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./23_dko/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./24_kesbangpol/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./25_satpolpp/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./26_bkppd/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./28_paringin/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./31_lampihong/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./32_batumandi/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./33_juai/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./34_awayan/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./35_halong/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./36_paringinselatan/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./38_tebingtinggi/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./39_bpbd/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./40_dpkp/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./41_disnakertrans/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./42_dppkb/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./43_kominfo/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./44_kearsipan/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./45_perikanan/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./46_pariwisata/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./47_perdagangan/daftar_pengadaan_skpd_p.xml
+sed -i 's/2019/2020/g' ./48_bppd/daftar_pengadaan_skpd_p.xml
+
+
+
+
+
+# Daftar Pengeluaran
+
+
+sed -i 's/2019/2020/g' ./01_setwan/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./02_setda/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./03_dpupr/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./04_dishub/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./05_dinkes/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./06_blud_rsud/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./07_disdik/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./08_perpustakaan/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./09_sosial/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./10_dpmd/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./11_dpppa/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./12_dukcatpil/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./13_pertanian/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./15_dkp/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./16_dkukmp/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./18_dpmptsp/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./19_bkd/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./20_inspektorat/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./21_bappeda/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./22_dlh/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./23_dko/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./24_kesbangpol/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./25_satpolpp/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./26_bkppd/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./28_paringin/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./31_lampihong/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./32_batumandi/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./33_juai/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./34_awayan/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./35_halong/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./36_paringinselatan/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./38_tebingtinggi/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./39_bpbd/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./40_dpkp/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./41_disnakertrans/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./42_dppkb/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./43_kominfo/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./44_kearsipan/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./45_perikanan/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./46_pariwisata/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./47_perdagangan/daftar_pengeluaran_skpd.xml
+sed -i 's/2019/2020/g' ./48_bppd/daftar_pengeluaran_skpd.xml
