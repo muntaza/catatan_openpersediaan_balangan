907e890adad3abcb8f5ba5c304762b4a7b205ad0
Muhammad Muntaza <muhammad@muntaza.id>
2020-07-27 11:09:52 +0800

    rubah tahun dari 2019 ke 2020

--- saldo_awal_2020/rsud/script_rubah.sh
+++ saldo_awal_2020/rsud/script_rubah.sh
@@ -1,2 +1,2 @@
-cat rsud_obat.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87044\,/g -e s/$/\)\;/g > rsud_obat_insert.sql
-cat rsud_bmhp.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87044\,/g -e s/$/\)\;/g > rsud_bmhp_insert.sql
+cat rsud_obat.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(101327\,/g -e s/$/\)\;/g > rsud_obat_insert.sql
+#cat rsud_bmhp.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87044\,/g -e s/$/\)\;/g > rsud_bmhp_insert.sql
