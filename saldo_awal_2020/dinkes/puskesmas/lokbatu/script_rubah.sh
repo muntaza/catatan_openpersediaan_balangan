bcf8d4e84759ec57e8557d38cf56c147b4423894
Muhammad Muntaza <muhammad@muntaza.id>
2020-12-23 06:08:23 +0800

    add

--- saldo_awal_2020/dinkes/puskesmas/lokbatu/script_rubah.sh
+++ saldo_awal_2020/dinkes/puskesmas/lokbatu/script_rubah.sh
@@ -1,2 +1,2 @@
-cat lokbatu_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(98608\,/g -e s/$/\)\;/g > lokbatu_masuk_insert.sql
-cat lokbatu_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(98609\,/g -e s/$/\)\;/g > lokbatu_keluar_insert.sql
+cat lokbatu_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(116316\,/g -e s/$/\)\;/g > lokbatu_masuk_insert.sql
+cat lokbatu_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(116317\,/g -e s/$/\)\;/g > lokbatu_keluar_insert.sql
