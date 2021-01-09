9d6266be93cc83b0b9247f3d006aadf388090fcc
Muhammad Muntaza <muhammad@muntaza.id>
2021-01-09 09:24:53 +0800

    add

--- saldo_awal_2020/rsud/sub_unit/apotik_rawat_jalan/script_rubah.sh
+++ saldo_awal_2020/rsud/sub_unit/apotik_rawat_jalan/script_rubah.sh
@@ -1,2 +1,2 @@
-cat apotik_rawat_jalan.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87046\,/g -e s/$/\)\;/g > apotik_rawat_jalan_insert.sql
-cat apotik_rawat_jalan_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(87047\,/g -e s/$/\)\;/g > apotik_rawat_jalan_keluar_insert.sql
+cat apotik_rawat_jalan_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(118951\,/g -e s/$/\)\;/g > apotik_rawat_jalan_masuk_insert.sql
+cat apotik_rawat_jalan_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(118952\,/g -e s/$/\)\;/g > apotik_rawat_jalan_keluar_insert.sql
