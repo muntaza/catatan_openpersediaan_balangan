ba3bc44a4ffa603b5fb802a285a0d9ac5099c300
Muhammad Muntaza <muhammad@muntaza.id>
2021-01-13 04:32:50 +0800

    update

---
+++ saldo_awal_2020/rsud/sub_unit/ruang_anak/ruang_anak_masuk_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (119909,650,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (119909,3976,1,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (119909,2620,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (119909,601,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (119909,502,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (119909,13290,3,0);
