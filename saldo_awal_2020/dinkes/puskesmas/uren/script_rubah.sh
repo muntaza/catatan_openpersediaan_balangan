480ac658ef7a296d1753365ec87680f8d06b501b
Muhammad Muntaza <muhammad@muntaza.id>
2021-01-13 04:51:53 +0800

    add

--- saldo_awal_2020/dinkes/puskesmas/uren/script_rubah.sh
+++ saldo_awal_2020/dinkes/puskesmas/uren/script_rubah.sh
@@ -1,2 +1,2 @@
-cat uren_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(98611\,/g -e s/$/\)\;/g > uren_masuk_insert.sql
-cat uren_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(98612\,/g -e s/$/\)\;/g > uren_keluar_insert.sql
+cat uren_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119911\,/g -e s/$/\)\;/g > uren_masuk_insert.sql
+cat uren_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119912\,/g -e s/$/\)\;/g > uren_keluar_insert.sql
