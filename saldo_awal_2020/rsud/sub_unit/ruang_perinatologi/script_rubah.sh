eb920a144029790f53cee9c7af839064184c037d
Muhammad Muntaza <muhammad@muntaza.id>
2021-01-13 04:23:26 +0800

    add

--- saldo_awal_2020/rsud/sub_unit/ruang_perinatologi/script_rubah.sh
+++ saldo_awal_2020/rsud/sub_unit/ruang_perinatologi/script_rubah.sh
@@ -1,2 +1,2 @@
-cat ruang_perinatologi_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(89631\,/g -e s/$/\)\;/g > ruang_perinatologi_masuk_insert.sql
-cat ruang_perinatologi_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(89632\,/g -e s/$/\)\;/g > ruang_perinatologi_keluar_insert.sql
+cat ruang_perinatologi_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119907\,/g -e s/$/\)\;/g > ruang_perinatologi_masuk_insert.sql
+cat ruang_perinatologi_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119908\,/g -e s/$/\)\;/g > ruang_perinatologi_keluar_insert.sql
