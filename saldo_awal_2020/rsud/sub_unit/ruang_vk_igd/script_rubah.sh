c90f2997ef3a68a73f783324ee604032abc61e88
Muhammad Muntaza <muhammad@muntaza.id>
2021-01-12 23:04:19 +0800

    add

--- saldo_awal_2020/rsud/sub_unit/ruang_vk_igd/script_rubah.sh
+++ saldo_awal_2020/rsud/sub_unit/ruang_vk_igd/script_rubah.sh
@@ -1,2 +1,2 @@
-cat ruang_vk_igd_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(89641\,/g -e s/$/\)\;/g > ruang_vk_igd_masuk_insert.sql
-cat ruang_vk_igd_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(89642\,/g -e s/$/\)\;/g > ruang_vk_igd_keluar_insert.sql
+cat ruang_vk_igd_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119902\,/g -e s/$/\)\;/g > ruang_vk_igd_masuk_insert.sql
+cat ruang_vk_igd_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119903\,/g -e s/$/\)\;/g > ruang_vk_igd_keluar_insert.sql
