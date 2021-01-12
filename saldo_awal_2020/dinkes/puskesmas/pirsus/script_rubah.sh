28e7e1226e0ea9b43fcd35e385988f96eaf495fe
Muhammad Muntaza <muhammad@muntaza.id>
2021-01-13 05:01:48 +0800

    add

--- saldo_awal_2020/dinkes/puskesmas/pirsus/script_rubah.sh
+++ saldo_awal_2020/dinkes/puskesmas/pirsus/script_rubah.sh
@@ -1,2 +1,2 @@
-cat pirsus_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(98613\,/g -e s/$/\)\;/g > pirsus_masuk_insert.sql
-cat pirsus_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(98614\,/g -e s/$/\)\;/g > pirsus_keluar_insert.sql
+cat pirsus_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119913\,/g -e s/$/\)\;/g > pirsus_masuk_insert.sql
+cat pirsus_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119914\,/g -e s/$/\)\;/g > pirsus_keluar_insert.sql
