8018a12cbfda6c0232590bcf4c08bce52829f819
Muhammad Muntaza <muhammad@muntaza.id>
2021-10-12 07:01:10 +0800

    update saldo awal

--- saldo_awal_2021/dinkes/puskesmas/juai/script_rubah.sh
+++ saldo_awal_2021/dinkes/puskesmas/juai/script_rubah.sh
@@ -1,2 +1,2 @@
-cat juai_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(115969\,/g -e s/$/\)\;/g > juai_masuk_insert.sql
-cat juai_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(115970\,/g -e s/$/\)\;/g > juai_keluar_insert.sql
+cat juai_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11138\,/g -e s/$/\)\;/g > juai_masuk_insert.sql
+cat juai_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11139\,/g -e s/$/\)\;/g > juai_keluar_insert.sql
