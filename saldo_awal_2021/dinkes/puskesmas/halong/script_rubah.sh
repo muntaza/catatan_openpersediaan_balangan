7d7d15e3afbcbe99a6a3c20682875339df77425a
Muhammad Muntaza <muhammad@muntaza.id>
2021-10-12 07:09:40 +0800

    update saldo awal

--- saldo_awal_2021/dinkes/puskesmas/halong/script_rubah.sh
+++ saldo_awal_2021/dinkes/puskesmas/halong/script_rubah.sh
@@ -1,2 +1,2 @@
-cat halong_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(116310\,/g -e s/$/\)\;/g > halong_masuk_insert.sql
-cat halong_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(116311\,/g -e s/$/\)\;/g > halong_keluar_insert.sql
+cat halong_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11140\,/g -e s/$/\)\;/g > halong_masuk_insert.sql
+cat halong_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11141\,/g -e s/$/\)\;/g > halong_keluar_insert.sql
