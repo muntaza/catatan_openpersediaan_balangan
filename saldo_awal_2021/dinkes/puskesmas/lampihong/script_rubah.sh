200f96de16cd4fa63ebb3498b03364466217fbe8
Muhammad Muntaza <muhammad@muntaza.id>
2021-10-12 06:33:00 +0800

    update saldo awal

--- saldo_awal_2021/dinkes/puskesmas/lampihong/script_rubah.sh
+++ saldo_awal_2021/dinkes/puskesmas/lampihong/script_rubah.sh
@@ -1,2 +1,2 @@
-cat lampihong_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(115626\,/g -e s/$/\)\;/g > lampihong_masuk_insert.sql
-cat lampihong_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(115627\,/g -e s/$/\)\;/g > lampihong_keluar_insert.sql
+cat lampihong_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11132\,/g -e s/$/\)\;/g > lampihong_masuk_insert.sql
+cat lampihong_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11133\,/g -e s/$/\)\;/g > lampihong_keluar_insert.sql
