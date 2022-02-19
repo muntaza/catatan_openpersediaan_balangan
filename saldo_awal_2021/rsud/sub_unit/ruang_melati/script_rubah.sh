a1821c2f2357620b7d649cba4eb69da0197f7811
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-19 18:04:08 +0800

    saldo awal ruang melati

--- saldo_awal_2021/rsud/sub_unit/ruang_melati/script_rubah.sh
+++ saldo_awal_2021/rsud/sub_unit/ruang_melati/script_rubah.sh
@@ -1,2 +1,2 @@
-cat ruang_melati_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(118976\,/g -e s/$/\)\;/g > ruang_melati_masuk_insert.sql
-cat ruang_melati_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(118977\,/g -e s/$/\)\;/g > ruang_melati_keluar_insert.sql
+cat ruang_melati_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17211\,/g -e s/$/\)\;/g > ruang_melati_masuk_insert.sql
+cat ruang_melati_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17212\,/g -e s/$/\)\;/g > ruang_melati_keluar_insert.sql
