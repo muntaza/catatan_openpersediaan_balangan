c36947fec8689ead5fc80b002316a31482b77c88
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-19 15:32:02 +0800

    saldo awal apotik rawat inap

--- saldo_awal_2021/rsud/sub_unit/apotik_rawat_inap/script_rubah.sh
+++ saldo_awal_2021/rsud/sub_unit/apotik_rawat_inap/script_rubah.sh
@@ -1,2 +1,2 @@
-cat apotik_rawat_inap_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119899\,/g -e s/$/\)\;/g > apotik_rawat_inap_masuk_insert.sql
-cat apotik_rawat_inap_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(119901\,/g -e s/$/\)\;/g > apotik_rawat_inap_keluar_insert.sql
+cat apotik_rawat_inap_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17203\,/g -e s/$/\)\;/g > apotik_rawat_inap_masuk_insert.sql
+cat apotik_rawat_inap_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17204\,/g -e s/$/\)\;/g > apotik_rawat_inap_keluar_insert.sql
