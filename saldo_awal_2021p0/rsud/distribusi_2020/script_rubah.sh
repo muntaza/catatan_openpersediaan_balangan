4462c3794df7957b0ca3259967edc0c9e4ae2005
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 07:05:02 +0800

    update data

--- saldo_awal_2021p0/rsud/distribusi_2020/script_rubah.sh
+++
@@ -1,1 +0,0 @@
-cat apotik_rawat_jalan.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(122080\,/g -e s/$/\)\;/g > apotik_rawat_jalan_insert.sql
