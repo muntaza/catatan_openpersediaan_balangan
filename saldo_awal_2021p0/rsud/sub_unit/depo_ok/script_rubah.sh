0d09bebafd04854739172cf607596896d95ea8b7
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 07:03:04 +0800

    saldo awal depo OK 2021 perubahan

--- saldo_awal_2021p0/rsud/sub_unit/depo_ok/script_rubah.sh
+++ saldo_awal_2021p0/rsud/sub_unit/depo_ok/script_rubah.sh
@@ -1,2 +1,2 @@
-cat depo_ok_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17205\,/g -e s/$/\)\;/g > depo_ok_masuk_insert.sql
-cat depo_ok_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17206\,/g -e s/$/\)\;/g > depo_ok_keluar_insert.sql
+cat depo_ok_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26204\,/g -e s/$/\)\;/g > depo_ok_masuk_insert.sql
+cat depo_ok_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26203\,/g -e s/$/\)\;/g > depo_ok_keluar_insert.sql
