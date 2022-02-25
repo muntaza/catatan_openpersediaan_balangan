5e79943f275aec6a894b7eb413c9d12e7b5758d5
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-25 17:50:22 +0800

    rsud saldo awal 2021 perubahan

--- saldo_awal_2021p0/rsud/script_rubah.sh
+++ saldo_awal_2021p0/rsud/script_rubah.sh
@@ -1,1 +1,2 @@
-cat rsud.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(2131\,/g -e s/$/\)\;/g > rsud_insert.sql
+cat rsud_obat.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26195\,/g -e s/$/\)\;/g > rsud_obat_insert.sql
+cat rsud_bahan_lainnya.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26196\,/g -e s/$/\)\;/g > rsud_bahan_lainnya_insert.sql
