0502ec9f8caa463d84b3892c475dd857b9548454
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 07:14:09 +0800

    saldo awal Ruang Kebidanan 2021 perubahan

--- saldo_awal_2021p0/rsud/sub_unit/ruang_kebidanan/script_rubah.sh
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_kebidanan/script_rubah.sh
@@ -1,2 +1,2 @@
-cat ruang_kebidanan_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17207\,/g -e s/$/\)\;/g > ruang_kebidanan_masuk_insert.sql
-cat ruang_kebidanan_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17208\,/g -e s/$/\)\;/g > ruang_kebidanan_keluar_insert.sql
+cat ruang_kebidanan_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26205\,/g -e s/$/\)\;/g > ruang_kebidanan_masuk_insert.sql
+cat ruang_kebidanan_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26206\,/g -e s/$/\)\;/g > ruang_kebidanan_keluar_insert.sql
