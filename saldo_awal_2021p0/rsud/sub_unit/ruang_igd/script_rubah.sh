7dbe21e8ce11e54e94ff946a4aac447d3eff2316
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 14:16:06 +0800

    saldo awal 2021 perubahan pada ruang IGD

--- saldo_awal_2021p0/rsud/sub_unit/ruang_igd/script_rubah.sh
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_igd/script_rubah.sh
@@ -1,2 +1,2 @@
-cat ruang_igd_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17215\,/g -e s/$/\)\;/g > ruang_igd_masuk_insert.sql
-cat ruang_igd_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17216\,/g -e s/$/\)\;/g > ruang_igd_keluar_insert.sql
+cat ruang_igd_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26223\,/g -e s/$/\)\;/g > ruang_igd_masuk_insert.sql
+cat ruang_igd_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26224\,/g -e s/$/\)\;/g > ruang_igd_keluar_insert.sql
