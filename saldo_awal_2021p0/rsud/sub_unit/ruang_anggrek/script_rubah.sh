d4e1e29d8df0cd0423bf64235ef137cac8768c26
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 14:41:59 +0800

    saldo awal 2021 perubahan pada ruang Anggrek

--- saldo_awal_2021p0/rsud/sub_unit/ruang_anggrek/script_rubah.sh
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_anggrek/script_rubah.sh
@@ -1,2 +1,2 @@
-cat ruang_anggrek_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17217\,/g -e s/$/\)\;/g > ruang_anggrek_masuk_insert.sql
-cat ruang_anggrek_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17218\,/g -e s/$/\)\;/g > ruang_anggrek_keluar_insert.sql
+cat ruang_anggrek_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26230\,/g -e s/$/\)\;/g > ruang_anggrek_masuk_insert.sql
+cat ruang_anggrek_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26231\,/g -e s/$/\)\;/g > ruang_anggrek_keluar_insert.sql
