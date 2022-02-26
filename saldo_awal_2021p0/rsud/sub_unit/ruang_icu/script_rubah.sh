2b574296f2f140cdb5a0d31b5632bee9c7697717
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 11:35:04 +0800

    saldo awal 2021 perubahan pada Ruang ICU

--- saldo_awal_2021p0/rsud/sub_unit/ruang_icu/script_rubah.sh
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_icu/script_rubah.sh
@@ -1,2 +1,2 @@
-cat ruang_icu_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17209\,/g -e s/$/\)\;/g > ruang_icu_masuk_insert.sql
-cat ruang_icu_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17210\,/g -e s/$/\)\;/g > ruang_icu_keluar_insert.sql
+cat ruang_icu_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26209\,/g -e s/$/\)\;/g > ruang_icu_masuk_insert.sql
+cat ruang_icu_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26210\,/g -e s/$/\)\;/g > ruang_icu_keluar_insert.sql
