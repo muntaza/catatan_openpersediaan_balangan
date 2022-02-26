8699abaad4a3ed237840c73b56c62279012caaff
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 17:48:11 +0800

    saldo awal 2021 perubahan pada PKM Batumandi

--- saldo_awal_2021p0/dinkes/puskesmas/batumandi/script_rubah.sh
+++ saldo_awal_2021p0/dinkes/puskesmas/batumandi/script_rubah.sh
@@ -1,2 +1,2 @@
-cat batumandi_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11134\,/g -e s/$/\)\;/g > batumandi_masuk_insert.sql
-cat batumandi_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11135\,/g -e s/$/\)\;/g > batumandi_keluar_insert.sql
+cat batumandi_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26249\,/g -e s/$/\)\;/g > batumandi_masuk_insert.sql
+cat batumandi_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26250\,/g -e s/$/\)\;/g > batumandi_keluar_insert.sql
