5eddc5c28583440c2c1ea0d6d0f61cf9907ff19a
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 17:19:58 +0800

    saldo awal 2021 perubahan pada ruang VK IGD

--- saldo_awal_2021p0/rsud/sub_unit/ruang_vk_igd/script_rubah.sh
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_vk_igd/script_rubah.sh
@@ -1,2 +1,2 @@
-cat ruang_vk_igd_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17225\,/g -e s/$/\)\;/g > ruang_vk_igd_masuk_insert.sql
-cat ruang_vk_igd_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17226\,/g -e s/$/\)\;/g > ruang_vk_igd_keluar_insert.sql
+cat ruang_vk_igd_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26243\,/g -e s/$/\)\;/g > ruang_vk_igd_masuk_insert.sql
+cat ruang_vk_igd_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26244\,/g -e s/$/\)\;/g > ruang_vk_igd_keluar_insert.sql
