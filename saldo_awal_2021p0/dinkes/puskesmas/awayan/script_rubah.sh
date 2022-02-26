d67c9978bf56a4a866a272e3b960d3e7c21b663d
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 21:45:29 +0800

    saldo awal 2021 perubahan pada PKM Awayan

--- saldo_awal_2021p0/dinkes/puskesmas/awayan/script_rubah.sh
+++ saldo_awal_2021p0/dinkes/puskesmas/awayan/script_rubah.sh
@@ -1,2 +1,2 @@
-cat awayan_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11136\,/g -e s/$/\)\;/g > awayan_masuk_insert.sql
-cat awayan_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11137\,/g -e s/$/\)\;/g > awayan_keluar_insert.sql
+cat awayan_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26252\,/g -e s/$/\)\;/g > awayan_masuk_insert.sql
+cat awayan_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26253\,/g -e s/$/\)\;/g > awayan_keluar_insert.sql
