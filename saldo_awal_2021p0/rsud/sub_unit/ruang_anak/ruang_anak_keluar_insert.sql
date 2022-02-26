c2872905b47942c2422fe2e6a7f916929df0b0c2
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 17:08:48 +0800

    saldo awal 2021 perubahan pada ruang Anak

---
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_anak/ruang_anak_keluar_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26242,650,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26242,2620,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26242,601,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26242,502,4,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26242,13290,3,0);
---
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_anak/ruang_anak_masuk_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26241,650,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26241,2620,5,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26241,601,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26241,502,4,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26241,13290,3,0);
