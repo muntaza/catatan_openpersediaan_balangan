41ec8dfc96f0221f59cea493ea3423f2ecbd1f69
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-26 15:47:23 +0800

    saldo awal 2021 perubahan pad ruang Perinatologi

---
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_perinatologi/ruang_perinatologi_keluar_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26240,650,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26240,3976,1,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26240,7903,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26240,502,4,0);
---
+++ saldo_awal_2021p0/rsud/sub_unit/ruang_perinatologi/ruang_perinatologi_masuk_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26239,650,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26239,3976,1,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26239,7903,2,0);
+INSERT INTO persediaan (id_transaksi, id_barang, jumlah, harga) VALUES (26239,502,4,0);
