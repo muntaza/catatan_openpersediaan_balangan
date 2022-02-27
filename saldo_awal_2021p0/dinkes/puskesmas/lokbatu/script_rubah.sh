fa6e20b1c9d5623a51301eb31e2e82dd6bfbdda4
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-27 08:48:41 +0800

    saldo awal 2021 perubahan pada PKM Lokbatu

--- saldo_awal_2021p0/dinkes/puskesmas/lokbatu/script_rubah.sh
+++ saldo_awal_2021p0/dinkes/puskesmas/lokbatu/script_rubah.sh
@@ -1,2 +1,2 @@
-cat lokbatu_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11242\,/g -e s/$/\)\;/g > lokbatu_masuk_insert.sql
-cat lokbatu_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(11243\,/g -e s/$/\)\;/g > lokbatu_keluar_insert.sql
+cat lokbatu_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26258\,/g -e s/$/\)\;/g > lokbatu_masuk_insert.sql
+cat lokbatu_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26259\,/g -e s/$/\)\;/g > lokbatu_keluar_insert.sql
