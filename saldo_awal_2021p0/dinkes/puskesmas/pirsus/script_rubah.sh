4a25bec48d7b50725aa54022dd7b4606e7afbec9
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-28 04:54:16 +0800

    saldo awal 2021 perubahan pada PKM Pirsus

--- saldo_awal_2021p0/dinkes/puskesmas/pirsus/script_rubah.sh
+++ saldo_awal_2021p0/dinkes/puskesmas/pirsus/script_rubah.sh
@@ -1,2 +1,2 @@
-cat pirsus_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(16981\,/g -e s/$/\)\;/g > pirsus_masuk_insert.sql
-cat pirsus_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(16982\,/g -e s/$/\)\;/g > pirsus_keluar_insert.sql
+cat pirsus_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26266\,/g -e s/$/\)\;/g > pirsus_masuk_insert.sql
+cat pirsus_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26267\,/g -e s/$/\)\;/g > pirsus_keluar_insert.sql
