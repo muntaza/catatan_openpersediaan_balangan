1262c76d2842598b7286bd80607919d64797fadd
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-28 06:47:29 +0800

    saldo awal 2021 perubahan pada PKM Paringin Selatan

--- saldo_awal_2021p0/dinkes/puskesmas/paringin_selatan/script_rubah.sh
+++ saldo_awal_2021p0/dinkes/puskesmas/paringin_selatan/script_rubah.sh
@@ -1,2 +1,2 @@
-cat paringin_selatan_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(16994\,/g -e s/$/\)\;/g > paringin_selatan_masuk_insert.sql
-cat paringin_selatan_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(16996\,/g -e s/$/\)\;/g > paringin_selatan_keluar_insert.sql
+cat paringin_selatan_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26268\,/g -e s/$/\)\;/g > paringin_selatan_masuk_insert.sql
+cat paringin_selatan_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26269\,/g -e s/$/\)\;/g > paringin_selatan_keluar_insert.sql
