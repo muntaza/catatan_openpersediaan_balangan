e1066b0db480ccb44b50d36dbf311a37c3480eb0
Muhammad Muntaza <muhammad@muntaza.id>
2022-02-28 11:16:08 +0800

    saldo awal 2021 perubahan pada PKM Tanah Habang

--- saldo_awal_2021p0/dinkes/puskesmas/tanah_habang/script_rubah.sh
+++ saldo_awal_2021p0/dinkes/puskesmas/tanah_habang/script_rubah.sh
@@ -1,2 +1,2 @@
-cat tanah_habang_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17061\,/g -e s/$/\)\;/g > tanah_habang_masuk_insert.sql
-cat tanah_habang_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(17062\,/g -e s/$/\)\;/g > tanah_habang_keluar_insert.sql
+cat tanah_habang_masuk.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26272\,/g -e s/$/\)\;/g > tanah_habang_masuk_insert.sql
+cat tanah_habang_keluar.sql | sed -e s/^/INSERT\ INTO\ persediaan\ \(id_transaksi\,\ id_barang\,\ jumlah\,\ harga\)\ VALUES\ \(26273\,/g -e s/$/\)\;/g > tanah_habang_keluar_insert.sql
